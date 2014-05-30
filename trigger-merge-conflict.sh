9fe59835cacefc0b99a2a7d7612ef730d6c68a4a
Tom Weiss <Tom.Weiss@TradingTechnologies.com>
2014-05-29 22:06:20 -0500

    conflict testing

--- trigger-merge-conflict.sh
+++ trigger-merge-conflict.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-git branch -d conflict-test-1
-git branch -d conflict-test-2
+git branch -D conflict-test-1
+git branch -D conflict-test-2
 git checkout -b conflict-test-1
-echo conflict-1 > conflict-test.txt
+echo conflict-1 >> conflict-test.txt
 git add conflict-test.txt
 git commit -m "conflict-test-1"
 git checkout master
 git checkout -b conflict-test-2
-echo conflict-2 > conflict-test.txt
+echo conflict-2 >> conflict-test.txt
 git add conflict-test.txt
 git commit -m "conflict-test-2"
-git merge conflict-1
+git merge conflict-test-1
 # (merge conflict)
-# May need to manually delete the branches when done.
+# git reset --merge (to abort the merge)
+# gits master
