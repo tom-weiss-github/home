ae21b544519f2399aaf1af2d28ba768a5d17c0e2
Tom Weiss <Tom.Weiss@TradingTechnologies.com>
2014-06-25 09:54:07 -0500

    create conflicts with many differences per file

--- trigger-merge-conflict.sh
+++ trigger-merge-conflict.sh
@@ -2,7 +2,6 @@
 
 # Execute as . ./trigger-merge-conflict.sh
 
-
 function write_contents()
 {
     if [ -z "$1" ]; then
@@ -11,17 +10,26 @@ function write_contents()
     fi
 
     local file_name=$1
+    truncate -s 0 $file_name
 
     for blocks in 1 2 3
     do
-        for index in 1 2 3 4 5
+        for index in 1 2 3 4 5 6 7 8 9 10
         do
-            echo "This line ($index) was added, but will be the same." >> $file_name
+            echo "This line ($blocks)-($index) but will be the same in both files." >> $file_name
         done
         openssl rand -base64 32 >> $file_name
         echo "" >> $file_name
     done
 }
+
+# This code was run once to create the files with content so that they have a common
+# ancestor with similar content.  I found if I just create files which are similar
+# on different branches then the entire file shows as a diff.  This will only need
+# to be done again if the write_contents function changes.
+# write_contents "./conflict-test-1.txt"
+# write_contents "./conflict-test-2.txt"
+# return
 
 git checkout master
 git branch -D conflict-test-alpha
