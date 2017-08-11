b1f43998fe08a4015948d8f09b71b7f3a96ef7ec
Tom Weiss <Tom.Weiss@TradingTechnologies.com>
2017-08-11 12:14:08 -0500

    script to put displays nice on wide screen

--- rhel/.bashrc
+++ rhel/.bashrc
@@ -227,7 +227,7 @@ alias envs='echo PATH $PATH; echo LD_LIBRARY_PATH $LD_LIBRARY_PATH; echo C_INCLU
 alias repo="python ~/githome/get-repo.py"
 alias mdbd='sudo mount -o user=intad/tweiss -t cifs //chifs01.int.tt.local/Share/Dead_By_Dawn /mnt/dbd/'
 alias kolmar='sudo mount.cifs -o user=weiss //192.168.1.117/Users /mnt/kolmar'
-alias gld2vm49='sudo mount.cifs -o user=tweiss,uid=1000,gid=1000 //10.192.2.49/shared /home/tweiss/gld2vm49'
+alias gld2vm49='sudo mount.cifs -o user=tweiss,uid=1000,gid=1000 //10.195.2.49/shared /home/tweiss/gld2vm49'
 alias cli_mt='run `git rev-parse --show-toplevel`/ext/linux/x86-64/release/bin/cli_mt 10.203.0.43:2181'
 alias jtrader="/usr/java/jdk1.7.0_03/bin/java -cp JTrader.jar JTrader &"
 alias ttr='`git rev-parse --show-toplevel`/run python `git rev-parse --show-toplevel`/t_trader/tt/ttrader/t_trader.py --stdout'
--- rhel/thirds.sh
+++ rhel/thirds.sh
@@ -1,26 +1,66 @@
 #!/bin/bash
 
-# These names are obtained from running 'wmctrl -lx'.
-chrome='google-chrome.Google-chrome'
-terminator='terminator.Terminator'
+function program_count()
+{
+    # The -x shows the WM_CLASS and that seems to be unique for applications.
+    application_count=`wmctrl -lx | grep -o $1 | wc -l`
+    if [[ $application_count != "0" ]] && [[ $application_count != "1" ]]; then
+        echo "Found $application_count instances of $1, there can be only 1 to resize."
+        exit 1
+    fi
+}
 
-# The -x shows the WM_CLASS and that seems to be unique for chrome.  Confirmed if there are two
-# chome instances then I get two values.  wmctrl -lx
-chrome_count=`wmctrl -lx | grep -o $chrome | wc -l`
-if [ $chrome_count != "1" ]; then
-    echo "Found $chrome_count instances of Chrome, there can be only 1 to resize."
-    exit 1
+# These names are obtained from running 'wmctrl -lx'.
+terminator='terminator.Terminator'
+emacs='emacs-24_3.Emacs'
+chrome='chromium-browser.Chromium-browser'
+
+# Some useful commands when callibrating this script.
+# wmctrl -lG will show x-offset, y-offset, width, and height.
+# xprop -id 0x00001234 (0x00001234 from wmctrl will list properties of the window).
+
+program_count $terminator
+program_count $emacs
+program_count $chrome
+
+terminator_id=`wmctrl -lx | grep $terminator | tr -s " " | cut -d " " -f 1`
+chrome_id=`wmctrl -lx | grep $chrome | tr -s " " | cut -d " " -f 1`
+# The 'grep -v xps' is there because I don't want to use emacs from my xps host, but from another
+# host.  Part of the output of the command contains the client host.
+emacs_id=`wmctrl -lx | grep $emacs | grep -v xps | tr -s " " | cut -d " " -f 1`
+
+echo terminator_id is $terminator_id
+echo chrome_id is $chrome_id
+echo emacs_id is $emacs_id
+
+if [ "$1" == "off" ]; then
+    wmctrl -r $terminator_id -i -b add,maximized_vert,maximized_horz
+    wmctrl -r $emacs_id -i -b add,maximized_vert,maximized_horz
+    wmctrl -r $chrome_id -i -b add,maximized_vert,maximized_horz
+    # Give chrome focus after we are done.
+    wmctrl -i -a $chrome_id
+    exit 0
 fi
 
-# Next: Let's make the error checking into a function and update it so that it's okay with either 0
-# or 1 instances.  Then invoke it for Terminator as well.
+if ! [ -z $terminator_id ]; then
+    # If a window is maximized, then its dimensions cannot be changed.  This will remove
+    # maximization if it's set.
+    wmctrl -r $terminator_id -i -b remove,maximized_vert,maximized_horz
+    wmctrl -r $terminator_id -i -e 0,0,0,1050,1387
+    wmctrl -r $terminator_id -i -b add,maximized_vert
+fi
 
+if ! [ -z $emacs_id ]; then
+    wmctrl -r $emacs_id -i -b remove,maximized_vert,maximized_horz
+    # wmctrl -r $emacs_id -i -e 0,1100,0,1130,1386
+    wmctrl -r $emacs_id -i -e 0,1110,0,1130,1386
+    wmctrl -r $emacs_id -i -b add,maximized_vert
+fi
 
-chrome_id=`wmctrl -lx | grep $chrome | tr -s " " | cut -d " " -f 1`
-echo $chrome_id
-
-# If a window is maximized, then its dimensions cannot be changed.
-wmctrl -r $chrome_id -i -b remove,maximized_vert,maximized_horz
-
-# Note there seems to be a bug in unity that leaves some space in the upper left when x,y are 0,0.
-wmctrl -r $chrome_id -i -e 0,0,0,600,600
+if ! [ -z $chrome_id ]; then
+    wmctrl -r $chrome_id -i -b remove,maximized_vert,maximized_horz
+    wmctrl -r $chrome_id -i -e 0,2240,0,1200,1367
+    wmctrl -r $chrome_id -i -b add,maximized_vert
+    # Give chrome focus after we are done.
+    wmctrl -i -a $chrome_id
+fi
